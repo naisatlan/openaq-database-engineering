92245ce5e8b24161fd8dd96e293a44a74d138131
Naïs Atlan <atlan.nais.06@gmail.com>
2025-12-28 18:19:13 +0100

    Remove nested git repo in ingestion folder

---
+++ sql_optimized/postgres/ingestion/database/postgres_writer.py
@@ -0,0 +1,71 @@
+from sqlalchemy import create_engine, text
+from config.settings import POSTGRES_URI
+import pandas as pd
+
+engine = create_engine(POSTGRES_URI)
+
+
+def write_locations(locations):
+    df = pd.DataFrame([{
+        "id": l["id"],
+        "city": l.get("locality") or l.get("name"),
+        "country": l["country"]["code"] if l.get("country") else None,
+        "latitude": l["coordinates"]["latitude"],
+        "longitude": l["coordinates"]["longitude"]
+    } for l in locations])
+
+    with engine.connect() as conn:
+        existing_ids = pd.read_sql(
+            text("SELECT id FROM location"),
+            conn
+        )["id"].tolist()
+
+    df = df[~df["id"].isin(existing_ids)]
+
+    if not df.empty:
+        df.to_sql("location", engine, if_exists="append", index=False)
+
+
+def write_sensors(sensors):
+    df = pd.DataFrame([{
+        "id": s["id"],
+        "location_id": s["location_id"],  # 🔑 obligatoire
+        "parameter_id": s.get("parameter", {}).get("id"),
+        "parameter": s.get("parameter", {}).get("name"),
+    } for s in sensors])
+
+    with engine.connect() as conn:
+        valid_locations = pd.read_sql(
+            text("SELECT id FROM location"),
+            conn
+        )["id"].tolist()
+
+    df = df[df["location_id"].isin(valid_locations)]
+
+    print("Sensors insérés :", len(df))
+
+    if not df.empty:
+        df.drop_duplicates("id").to_sql(
+            "sensor",
+            engine,
+            if_exists="append",
+            index=False
+        )
+
+
+def write_measurements(df):
+    if df.empty:
+        return
+
+    with engine.connect() as conn:
+        valid_sensors = pd.read_sql(
+            text("SELECT id FROM sensor"),
+            conn
+        )["id"].tolist()
+
+    df = df[df["sensor_id"].isin(valid_sensors)]
+
+    print("Measurements insérées :", len(df))
+
+    if not df.empty:
+        df.to_sql("measurement", engine, if_exists="append", index=False)
